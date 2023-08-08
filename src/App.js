9d4510ba34dd53d127578dc57b3d3459b29a93cb
abhi267266 <abhi267266@gmail.com>
2023-08-08 14:25:33 +0530

    first commit after intalization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MovieCard from "./MovieCard";
+
+function App() {
+  return (
+    <>
+    
+    <h1>working!!!!!!!</h1>
+    <MovieCard/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MovieCard.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class MovieCard extends Component{
+    render(){
+        return (
+            <div className='main'>
+                <div className="movie-card"></div>
+            </div>
+        )
+    }
+}
